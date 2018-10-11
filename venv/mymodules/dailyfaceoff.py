96c846d67102358f25d0997b424afd1ac1efd3d0
home284 <Fgjkjy13>
2018-10-11 17:43:48 +0300

    commit message

--- venv/mymodules/dailyfaceoff.py
+++ venv/mymodules/dailyfaceoff.py
@@ -1,3 +1,22 @@
-from urllib import urlopen
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+def downloadPage(url):
+    html = urlopen(url)
+    bs = BeautifulSoup(html, 'html.parser')
+    return bs
+
+def parseTeamLines(url, id):
+    bs = downloadPage(url)
+    div = bs.find('div', {'class': 'team-line-combination-wrap'})
+    for table in div.findAll('table', {'id': id}):
+        for tr in table.tbody.findAll('tr'):
+            print(tr.attrs['id'])
+            for td in tr.findAll('td'):
+                if 'id' not in td.attrs: continue
+                print('\t'+td.attrs['id'])
+
+parseTeamLines('https://www.dailyfaceoff.com/teams/montreal-canadiens/line-combinations/', 'forwards')
 
 print('DONE')
